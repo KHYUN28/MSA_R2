5196015e757c95ebce9bae3c65b931417ea28cf6
KKHYUN <kkh30123@naver.com>
2023-10-18 10:17:04 +0900

    Modified

--- chapter7/licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ chapter7/licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -15,11 +15,11 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-// @EnableEurekaClient 더이상 사용안하고 종속성만 추가하면 됨.
 @SpringBootApplication
 @RefreshScope
 @EnableDiscoveryClient
 @EnableFeignClients
+//@EnableEurekaClient
 public class LicenseServiceApplication {
 
 	public static void main(String[] args) {
